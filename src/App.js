6d9dbc4a2d7ba04555e2c75c191183288a5f225e
Santiago Olivera <lpcsaanty@gmail.com>
2022-05-30 20:01:05 -0300

    Actualizando Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  return (<>
-    <h1>Este es el comienzo de mi pagina en React</h1>
-  </>);
+  return <>
+    <Navbar />
+  </>
+
 }
 
 export default App;
